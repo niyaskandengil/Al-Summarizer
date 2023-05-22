887ca1924e882eab2ade6e157019f46c07dc931a
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 14:28:51 +0530

    #

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,42 +2,59 @@ import { useEffect, useState } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
-
 const Demo = () => {
-    const [article, setArticle] = useState({
-        url: '',
-        summary: '',
-    });
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+  const [allArticles, setAllArticles] = useState([]);
 
-    const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
-    const handleSubmit = async (e) => {
-        const {data} = await getSummary(articleUrl: article.url);
+  const handleSubmit = async (e) => {
+    e.preventDefault;
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      setAllArticles(updatedAllArticles);
+      setArticle(newArticle);
+
+      console.log(newArticle);
     }
+  };
   return (
     <section className="mt-16 w-full max-w-xl">
       {/*Search Bar*/}
       <div className="flex flex-col w-full gap-2">
-        <form
-          className="relative flex justify-center"
-          onSubmit={handleSubmit}
-        >
-            <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5"/>
-            <input
+        <form className="relative flex justify-center" onSubmit={handleSubmit}>
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
             type="url"
             placeholder="Enter a URL"
             value={article.url}
-            onChange={(e) => {setArticle({...article, url: e.target.value})}}
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
             required
             className="url_input peer "
-            />
-            <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
-              ↵ 
-            </button>
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↵
+          </button>
         </form>
-        {/*Browse URL History*/}    
+        {/*Browse URL History*/}
       </div>
-      {/* Display Result*/  }
+      {/* Display Result*/}
     </section>
   );
 };
