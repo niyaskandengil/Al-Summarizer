4f98d356d040b5561459255204732b9b904febee
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 13:19:36 +0530

    store

--- src/services/store.js
+++ src/services/store.js
@@ -1,1 +1,6 @@
-impor
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer: {},
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat()
+});  
