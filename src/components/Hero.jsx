23771c465d5224f4e7d40e765418fb05841e9a4b
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 12:48:23 +0530

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,24 @@ import logo from "../assets/logo.svg";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-       <nav className="flex justify-between items-center flex-col">
-         <img src={logo} alt="sumz_logo" className="w-28" />
-       </nav>
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() => {
+            window.open("http://github.com/niyaskandengil");
+          }}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Article with <br className="max-md:hidden"/>
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">Simplify your</h2>
     </header>
   );
 };
