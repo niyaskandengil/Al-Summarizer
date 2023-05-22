b2f79db519ae93b29dcaeb42a50b028f39a71680
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 13:09:27 +0530

    Demo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,13 +2,20 @@ import { useEffect, useState } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+    const [artilce, setArticle] = useState({
+        url: '',
+        summary: '',
+    });
+    const handleSubmit = async (e) => {
+        alert('submitted');
+    }
   return (
     <section className="mt-16 w-full max-w-xl">
       {/*Search Bar*/}
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
             <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5"/>
             <input
@@ -23,7 +30,9 @@ const Demo = () => {
               ↵ 
             </button>
         </form>
+        {/*Browse URL History*/}    
       </div>
+      {/* Display Result*/  }
     </section>
   );
 };
