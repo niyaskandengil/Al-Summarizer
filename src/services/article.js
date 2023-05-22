afe7aa24bebc74e14b1b2a0790b2683787182087
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 13:33:02 +0530

    #

--- src/services/article.js
+++ src/services/article.js
@@ -1,8 +1,10 @@
-import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3000'}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3000'}),
     endpoints: (builder) => ({
         getSummary: builder.query({
             query: (params) => 'test'})
