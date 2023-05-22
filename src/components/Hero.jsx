d277e18bb50abbe4a7bd663ea9525322515d991e
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 12:51:38 +0530

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,10 +17,13 @@ const Hero = () => {
         </button>
       </nav>
       <h1 className="head_text">
-        Summarize Article with <br className="max-md:hidden"/>
+        Summarize Article with <br className="max-md:hidden" />
         <span className="orange_gradient">OpenAI GPT-4</span>
       </h1>
-      <h2 className="desc">Simplify your</h2>
+      <h2 className="desc">
+        Simplify your reading with summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries.
+      </h2>
     </header>
   );
 };
