f6bb411d931ca811926ab3fbb9953712359264f4
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 12:24:08 +0530

    #

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.svg";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      Hero
+    </header>
+  );
+};
+
+export default Hero;
