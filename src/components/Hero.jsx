58c0aa4095bb0336541c71b005c68cb8d2de49cf
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-22 12:27:01 +0530

    logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,9 @@ import logo from "../assets/logo.svg";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      Hero
+       <nav className="flex justify-between items-center flex-col">
+         <img src={logo} alt="sumz_logo" className="w-28" />
+       </nav>
     </header>
   );
 };
